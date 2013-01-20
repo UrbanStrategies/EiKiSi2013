b8231736c64c15f0c3d2eea38322ec46007551f3
Spike <spjika@gmail.com>
2013-01-19 16:19:09 -0800

    Update source/maps_lib.js

--- source/maps_lib.js
+++ source/maps_lib.js
@@ -19,7 +19,6 @@ var MapsLib = {
   //the encrypted Table ID of your Fusion Table (found under File => About)
   //NOTE: numeric IDs will be depricated soon
   fusionTableId:      "1vFge162nQNKIaLIKati1K4860hipj_LXRNcF79w",
-  tierDiffTableId:    "1c8_4xQV7Vw21m5kDZqnD7Kz_QCOdrlXyF_RU4gc",
 
   //*New Fusion Tables Requirement* API key. found at https://code.google.com/apis/console/
   //*Important* this key is for demonstration purposes. please register your own.
